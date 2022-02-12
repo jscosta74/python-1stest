b5704019da8036557b3bc9923ed4ee2c338f586a
joses <jose.santos.costa@gmail.com>
2022-02-12 15:00:38 +0000

    mais uma alteracao

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 # mais uma alteracao
+# alteracao numero 2
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
